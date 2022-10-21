01c264afc3a370c94649847ce72844d498312bf9
Chi Nguyen <nglinhchi65@gmail.com>
2022-10-21 13:13:01 +0300

    adding functional buttons

--- src/App.js
+++ src/App.js
@@ -1,51 +1,29 @@
-const Header = (props) => {
-    return (
-      <>
-        <p>
-          {props.course}
-        </p>
-      </>
-    )
-  }
-  
-  const Content = (props) => {
-    return (
-      <>
-        <p>
-          {props.part1} {props.exercise1} <br /> 
-          {props.part2} {props.exercises2} <br /> 
-          {props.part3} {props.exercises3} <br />
-        </p>
-      </>
-    )
-  }
-  
-  const Total = (props) => {
-    return (
-      <>
-        <p>
-          Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
-        </p>
-      </>
-    )
-  }
-  
-  const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-  
-    return (
-      <div>
-        <Header course={course} />
-        <Content part1={part1} part2={part2} part3 = {part3} exercise1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      </div>
-    )
-  }
-  
-  export default App
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+  return (
+    <div>
+      <div>{counter}</div>
+      <Button onClick={increaseByOne}
+              text='plus' />
+      <Button onClick={decreaseByOne}
+              text='minus' />
+      <Button onClick={setToZero}
+              text='zero' />        
+    </div>
+    
+  )
+}
+
+export default App
